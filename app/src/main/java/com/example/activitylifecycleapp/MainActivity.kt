b90c4fa68b48dd29fd3050e509987d33e637da8b
ShahadNasser <Shahad.N.Alhanaya@gmail.com>
2021-09-28 12:34:51 +0300

    added floatingButton

--- app/src/main/java/com/example/activitylifecycleapp/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycleapp/MainActivity.kt
@@ -3,11 +3,31 @@ package com.example.activitylifecycleapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    lateinit var floatingActionButton: FloatingActionButton
+    lateinit var textView: TextView
+    var flag = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        textView = findViewById(R.id.textView)
+        floatingActionButton = findViewById(R.id.floatingActionButton)
+        floatingActionButton.setOnClickListener {
+            if(flag){
+                flag = !flag
+                textView.setTextColor(ContextCompat.getColor(this,R.color.purple_200))
+            }else{
+                flag = !flag
+                textView.setTextColor(ContextCompat.getColor(this,R.color.black))
+
+            }
+        }
 
         Log.d("MainActivity", "This is onCreate")
     }
